3f21c62a7afed657fb80eca29f6e38d138215e99
Maria Opland <mariaopl@stud.ntnu.no>
2021-09-09 13:40:56 +0200

    Task1.py modified

--- task1.py
+++ task1.py
@@ -1,16 +1,9 @@
 def highestProduct(listOfIntegers):
-    result = []
     product = 1
-    for i in range(0,3):
-        max1 = 0
-        for j in range(len(listOfIntegers)):
-            if listOfIntegers[j] > max1:
-                max1 = listOfIntegers[j]
-        listOfIntegers.remove(max1)
-        result.append(max1)
-    for k in range(len(result)):
-        product = product * result[k]
+    listOfIntegers.sort(reverse=True)
+    for i in range(3):
+        product = product * listOfIntegers[i]
     return product
 
+  
 
-print(highestProduct([20,70,4,7,10,65]))
